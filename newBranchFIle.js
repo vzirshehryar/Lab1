282ee4f766523db89910896c943d6f5884880c80
vzirshehryar <vzirshehryar@gmail.com>
2023-09-18 19:45:34 +0500

    modified file

--- newBranchFIle.js
+++ newBranchFIle.js
@@ -2,3 +2,5 @@ console.log("hello new brach");
 console.log("hello from new branchn let resolve conflicts");
 console.log("hello new brach");
 console.log("hello new brach");
+console.log("hello new brach");
+console.log("hello new brach");
